011b2d03a3f481ee5557760f8b61916b4d970ac1
leti-oli <o.leticiaa29@gmail.com>
2026-06-15 20:18:37 -0300

    port

---
+++ index.js
@@ -0,0 +1,9 @@
+const botao = document.getElementById("btnCuriosidade");
+const texto = document.getElementById("surpresa");
+
+botao.addEventListener("click", function(){
+
+    texto.innerHTML =
+    "🌌 Curiosidade surpresa: Sei tocar teclado, flauta doce e violão";
+
+});
